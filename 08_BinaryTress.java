ebeccb07d71fe9daf0093e5bbf39af5d504246bf
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-06-15 12:45:53 +0530

    Binary Tree Maximum Path Sum

--- 08_BinaryTress.java
+++ 08_BinaryTress.java
@@ -185,18 +185,66 @@ class Solution {
         // Input: root = [1,2]
         // Output: 1
 
-        int diameter = 0;
-        public int diameterOfBinaryTree(TreeNode root) {
-            height(root);
-            return diameter;
+        // int diameter = 0;
+        // public int diameterOfBinaryTree(TreeNode root) {
+        //     height(root);
+        //     return diameter;
+        // }
+        // public int height(TreeNode root) {
+        //     if(root == null) {
+        //         return 0;
+        //     }
+        //     int left = height(root.left);
+        //     int right = height(root.right);
+        //     diameter = Math.max(diameter, left + right);
+        //     return 1 + Math.max(left, right);
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //BINARY TREE MAXIMUM PATH SUM
+        // A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+        // The path sum of a path is the sum of the node's values in the path.
+        // Given the root of a binary tree, return the maximum path sum of any non-empty path.
+
+        // Example 1:
+        // Input: root = [1,2,3]
+        // Output: 6
+        // Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+        
+        // Example 2:
+        // Input: root = [-10,9,20,null,null,15,7]
+        // Output: 42
+        // Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+
+
+        int see = Integer.MIN_VALUE;
+        public int maxPathSum(TreeNode root) {
+            solve(root);
+            return see;
         }
-        public int height(TreeNode root) {
+        public int solve(TreeNode root) {
             if(root == null) {
                 return 0;
             }
-            int left = height(root.left);
-            int right = height(root.right);
-            diameter = Math.max(diameter, left + right);
-            return 1 + Math.max(left, right);
+            int left = Math.max(0, solve(root.left));
+            int right = Math.max(0, solve(root.right));
+            int temp = left + right + root.val;
+            see = Math.max(see, temp);
+            return root.val + Math.max(left, right);
         }
 }

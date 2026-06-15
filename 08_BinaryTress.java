057a6f84271e1b6c19dfc4dabc0ca277d4af80bc
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-06-15 11:22:03 +0530

    balanced binary tree check

--- 08_BinaryTress.java
+++ 08_BinaryTress.java
@@ -113,14 +113,56 @@ class Solution {
 
 //     Given the root of a binary tree, return its maximum depth.
 //     A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
-        public int maxDepth(TreeNode root) {
+        // public int maxDepth(TreeNode root) {
+        //     if(root == null) {
+        //         return 0;
+        //     }
+
+        //     int left = maxDepth(root.left);
+        //     int right = maxDepth(root.right);
+
+        //     return 1 + Math.max(left, right);
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //BALANCED BINARY TREE
+        // Given a binary tree, determine if it is height-balanced.
+
+        // Example 1:
+        // Input: root = [3,9,20,null,null,15,7]
+        // Output: true
+
+        public boolean isBalanced(TreeNode root) {
+            return height(root) != -1;
+        }
+        public int height(TreeNode root) {
             if(root == null) {
                 return 0;
             }
-
-            int left = maxDepth(root.left);
-            int right = maxDepth(root.right);
-
+            int left = height(root.left);
+            if(left == -1) {
+                return -1;
+            }
+            int right = height(root.right);
+            if(right == -1) {
+                return -1;
+            }
+            if(Math.abs(left - right) > 1) {
+                return -1;
+            }
             return 1 + Math.max(left, right);
         }
 }

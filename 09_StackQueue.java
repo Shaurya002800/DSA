59686a4ccf26e4da89bd6333e57abade50906991
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-07-06 00:07:51 +0530

    next greater element 1

--- 09_StackQueue.java
+++ 09_StackQueue.java
@@ -228,36 +228,111 @@ import java.util.*;
 // You must implement a solution with O(1) time complexity for each function.
 
 
-class MinStack {
-    Stack<Integer> stack;
-    Stack<Integer> minStack;
+// class MinStack {
+//     Stack<Integer> stack;
+//     Stack<Integer> minStack;
 
-    public MinStack() {
-        stack = new Stack<>();
-        minStack = new Stack<>();
-    }
+//     public MinStack() {
+//         stack = new Stack<>();
+//         minStack = new Stack<>();
+//     }
     
-    public void push(int value) {
-        stack.push(value);
+//     public void push(int value) {
+//         stack.push(value);
 
-        if(minStack.empty() || value <= minStack.peek()) {
-            minStack.push(value);
+//         if(minStack.empty() || value <= minStack.peek()) {
+//             minStack.push(value);
+//         }
+//     }
+    
+//     public void pop() {
+//         int removed = stack.pop();
+
+//         if(removed == minStack.peek()) {
+//             minStack.pop();
+//         }
+//     }
+    
+//     public int top() {
+//         return stack.peek();
+//     }
+    
+//     public int getMin() {
+//         return minStack.peek();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Next Greater Element I
+
+
+
+// The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+// You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+// For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+// Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+// Output: [-1,3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+// - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+
+
+
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int i = nums2.length - 1; i >= 0; i--) {
+            int curr = nums2[i];
+            while(!stack.empty() && stack.peek() <= curr) {
+                stack.pop();
+            }
+            if(stack.empty()) {
+                map.put(curr, -1);
+            } 
+            else {
+                map.put(curr, stack.peek());
+            }
+            stack.push(curr);
         }
-    }
-    
-    public void pop() {
-        int removed = stack.pop();
-
-        if(removed == minStack.peek()) {
-            minStack.pop();
+        int[] ans = new int[nums1.length];
+        for(int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
         }
-    }
-    
-    public int top() {
-        return stack.peek();
-    }
-    
-    public int getMin() {
-        return minStack.peek();
+        return ans;
     }
 }

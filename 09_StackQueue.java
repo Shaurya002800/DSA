ca6e43e437b6ce6261d5ba70e3ffcdc335e8bcdc
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-07-20 22:42:03 +0530

    Sum of Subarray Ranges

--- 09_StackQueue.java
+++ 09_StackQueue.java
@@ -367,27 +367,104 @@ import java.util.*;
 
 
 
+// class Solution {
+//     public int sumSubarrayMins(int[] arr) {
+//         int MOD = 1000000007;
+//         int n = arr.length;
+//         Stack<Integer> stack = new Stack<>();
+//         long ans = 0;
+//         for(int i = 0; i <= n; i++) {
+//             while(!stack.empty() && (i == n || arr[stack.peek()] >= arr[i])) {
+//                 int mid = stack.pop();
+//                 int left;
+//                 if(stack.empty()) {
+//                     left = -1;
+//                 } else {
+//                     left = stack.peek();
+//                 }
+//                 int right = i;
+//                 long count = (long)(mid - left) * (right - mid);
+//                 ans = (ans + arr[mid] * count) % MOD;
+//             }
+//             stack.push(i);
+//         }
+//         return (int) ans;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Sum of Subarray Ranges
+
+//  You are given an integer array nums. The range of a subarray of nums is the difference between the largest and smallest element in the subarray.
+// Return the sum of all subarray ranges of nums.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: 4
+// Explanation: The 6 subarrays of nums are the following:
+// [1], range = largest - smallest = 1 - 1 = 0 
+// [2], range = 2 - 2 = 0
+// [3], range = 3 - 3 = 0
+// [1,2], range = 2 - 1 = 1
+// [2,3], range = 3 - 2 = 1
+// [1,2,3], range = 3 - 1 = 2
+// So the sum of all ranges is 0 + 0 + 0 + 1 + 1 + 2 = 4.
+
+
+
+
+import java.util.*;
+
 class Solution {
-    public int sumSubarrayMins(int[] arr) {
-        int MOD = 1000000007;
-        int n = arr.length;
-        Stack<Integer> stack = new Stack<>();
+
+    public long subArrayRanges(int[] nums) {
+        return sumMax(nums) - sumMin(nums);
+    }
+
+    private long sumMin(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
         long ans = 0;
-        for(int i = 0; i <= n; i++) {
-            while(!stack.empty() && (i == n || arr[stack.peek()] >= arr[i])) {
-                int mid = stack.pop();
-                int left;
-                if(stack.empty()) {
-                    left = -1;
-                } else {
-                    left = stack.peek();
-                }
+
+        for (int i = 0; i <= n; i++) {
+            while (!st.isEmpty() && (i == n || nums[st.peek()] >= nums[i])) {
+                int mid = st.pop();
+                int left = st.isEmpty() ? -1 : st.peek();
                 int right = i;
-                long count = (long)(mid - left) * (right - mid);
-                ans = (ans + arr[mid] * count) % MOD;
+                ans += (long) nums[mid] * (mid - left) * (right - mid);
             }
-            stack.push(i);
+            st.push(i);
         }
-        return (int) ans;
+
+        return ans;
+    }
+
+    private long sumMax(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        long ans = 0;
+
+        for (int i = 0; i <= n; i++) {
+            while (!st.isEmpty() && (i == n || nums[st.peek()] <= nums[i])) {
+                int mid = st.pop();
+                int left = st.isEmpty() ? -1 : st.peek();
+                int right = i;
+                ans += (long) nums[mid] * (mid - left) * (right - mid);
+            }
+            st.push(i);
+        }
+
+        return ans;
     }
 }

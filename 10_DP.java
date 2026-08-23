4313059d62bfc99c1d66773b24275854c1e000de
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-08-23 23:59:23 +0530

    House robber

--- 10_DP.java
+++ 10_DP.java
@@ -19,22 +19,67 @@ import java.util.*;
 
 
 
+// class Solution {
+//     public int climbStairs(int n) {
+//         int t1 = 1;
+//         int t2 = 2;
+//         if(n == 1){
+//             return t1;
+//         }
+//         if(n == 2){
+//             return t2;
+//         }
+//         int summ = 0;
+//         for(int i = 3; i <= n; i++){
+//             summ = t1 + t2;
+//             t1 = t2;
+//             t2 = summ;
+//         }
+//         return summ;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//House Robber
+
+// ou are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+// Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+
+
 class Solution {
-    public int climbStairs(int n) {
-        int t1 = 1;
-        int t2 = 2;
-        if(n == 1){
-            return t1;
+    public int rob(int[] nums) {
+        int prevRob = 0;
+        int maxRob = 0;
+
+        for (int curValue : nums) {
+            int temp = Math.max(maxRob, prevRob + curValue);
+            prevRob = maxRob;
+            maxRob = temp;
         }
-        if(n == 2){
-            return t2;
-        }
-        int summ = 0;
-        for(int i = 3; i <= n; i++){
-            summ = t1 + t2;
-            t1 = t2;
-            t2 = summ;
-        }
-        return summ;
+
+        return maxRob;        
     }
 }

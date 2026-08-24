4f07e9f29424f36bfb14b97549898cce3fd87b75
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-08-24 23:28:32 +0530

    robber 2

--- 10_DP.java
+++ 10_DP.java
@@ -69,17 +69,67 @@ import java.util.*;
 // Total amount you can rob = 1 + 3 = 4.
 
 
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n];
+//         dp[0] = nums[0];
+//         int take = 0;
+//         int nottake = 0;
+//         for(int i = 1; i < n; i++){
+//             take = nums[i];
+//             if(i > 1) take += dp[i - 2];
+//             nottake = 0 + dp[i - 1];
+//             dp[i] = Math.max(take, nottake);
+//         }
+//         return dp[n - 1];
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+//Robber II
+
+//Circular mai houses and first andd last house are adjacent so we can not rob both of them. So we will take max of robbing first house and not robbing last house and robbing last house and not robbing first house.
+
+
+
 class Solution {
     public int rob(int[] nums) {
-        int prevRob = 0;
-        int maxRob = 0;
-
-        for (int curValue : nums) {
-            int temp = Math.max(maxRob, prevRob + curValue);
-            prevRob = maxRob;
-            maxRob = temp;
-        }
-
-        return maxRob;        
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0], nums[1]);
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int take = 0;
+        int ntake = 0;
+        int p = 0;
+        int t = 0;
+        for(int i = 1; i < n - 1; i++){
+            take = nums[i];
+            if(i > 1) take += dp[i - 2];
+            ntake = 0 + dp[i - 1];
+            dp[i] = Math.max(take, ntake);
+        } 
+        p = dp[n - 2];
+        take = 0;
+        ntake = 0;
+        dp[0] = 0;
+        dp[1] = nums[1];
+        for(int i = 2; i < n; i++){
+            take = nums[i];
+            if(i > 1) take += dp[i - 2];
+            ntake = 0 + dp[i - 1];
+            dp[i] = Math.max(take, ntake);
+        } 
+        t = dp[n - 1];
+        return Math.max(p, t);
     }
 }

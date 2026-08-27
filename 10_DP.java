68a03ccdd71c5fd94fcf5d7516714b497102f1ac
Shaurya002800 <kunwarshaurya28@gmail.com>
2026-08-27 19:38:48 +0530

    Unique path 2

--- 10_DP.java
+++ 10_DP.java
@@ -259,21 +259,72 @@ import java.util.*;
 
 //Tabulation Approach (Bottom-Up DP) with extra space of O(m*n) for dp array
 
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         for(int i = 0; i < m; i++){
+//             int up = 0;
+//             int left = 0;
+//             for(int j = 0; j < n; j++){
+//                 if(i == 0 && j == 0) dp[i][j] = 1;
+//                 else{
+//                     if(i > 0) up = dp[i - 1][j];
+//                     if(j > 0) left = dp[i][j - 1];
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+//         return dp[m - 1][n - 1];
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Unique Paths II
+
+// You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+// An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+// Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+
+
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
         for(int i = 0; i < m; i++){
-            int up = 0;
-            int left = 0;
             for(int j = 0; j < n; j++){
-                if(i == 0 && j == 0) dp[i][j] = 1;
+                if(obstacleGrid[i][j] == 1){
+                    obstacleGrid[i][j] = 0;
+                    continue;
+                }
+                if(i == 0 && j == 0) obstacleGrid[i][j] = 1;
                 else{
-                    if(i > 0) up = dp[i - 1][j];
-                    if(j > 0) left = dp[i][j - 1];
-                    dp[i][j] = up + left;
+                    int up = 0;
+                    int left = 0;
+                    if(i > 0) up = obstacleGrid[i - 1][j];
+                    if(j > 0) left = obstacleGrid[i][j - 1];
+                    obstacleGrid[i][j] = up + left;
                 }
             }
         }
-        return dp[m - 1][n - 1];
+        return obstacleGrid[m - 1][n - 1];
     }
 }
